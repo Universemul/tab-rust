05b2fa648faf5df49f67a938f928ae6ef1653531
Universemul <davidbarthelemy28@gmail.com>
2020-06-09 09:33:19 +0200

    update main

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,7 @@ fn main() {
     let mut rdr = reader::ContextReader::new().set_delimiter(b';').from_path("test.csv").unwrap();
     for line in rdr.lines()  {
         println!("{:?}", line);
-    }   
-    //println!("{:?}", rdr);
+    }
 }
 
 /*
